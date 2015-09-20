10d54079dbbc1e9b79b85c2207cdb6ea79a9fd3c
nicholopolis <nhrivard@gmail.com>
2015-09-20 15:03:38 -0400

    client buffer clearing enabled

--- client.c
+++ client.c
@@ -5,43 +5,56 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <signal.h>
-
-int main( int args, char argv[] ) {
-
+int main( int args, char *argv[] ) {
 // create socket
 int clisock;
 clisock = socket( AF_INET, SOCK_STREAM, 0 );
 
-
 // create client socket address
 struct sockaddr_in servaddr;
 memset( &servaddr, 0, sizeof(servaddr) );
-servaddr.sin_family      = AF_INET;
- servaddr.sin_port = htons(5000);
-servaddr.sin_addr.s_addr = inet_addr("172.17.85.252");
+servaddr.sin_family = AF_INET;
+servaddr.sin_port = htons(8888);
+servaddr.sin_addr.s_addr = inet_addr("172.17.48.51");
+//printf("yomamma\n");
 
- printf("yomamma\n");
- 
 // connect to server once accepted
-connect( clisock, (struct sockaddr *)&servaddr, sizeof(servaddr) );
+if (connect( clisock, (struct sockaddr *)&servaddr, sizeof(servaddr) ) < 0){
+  printf("connection failed\n");
+  return 1;
+}
 
- printf("did i connect?\n");
- 
-// send "Hello"
-char buf[10];
-strcpy( buf, "Hello\n" );
+printf("connected\n");
 
- printf("string?\n");
+//keep communication with server 
+while(1){
 
- send( clisock, (void *)buf, strlen(buf), 0 );
+  printf("Enter commands\n");
+  char buf[6000];
+  scanf("%s", buf);
+  printf("\n");
+  ssize_t bytesSent = send( clisock, buf, strlen(buf), 0 );
+  memset(buf, 0, sizeof(buf));
+  if(bytesSent < 0){
+    printf("send failed");
+    return 1;
+  }
+  printf("sent message of size %d\n", bytesSent);
 
- printf("omg!\n");
+  char buf2[6000];
+
+  if(recv(clisock, buf2, 6000, 0) < 0){
+    printf("receive failed\n");
+    return 1;
+  }
+  printf("server reply\n");
+  printf("%s\n", buf2);
+  memset(buf2, 0, sizeof(buf2));
+
+}
 
 // close socket, once finished
 close( clisock );
 
- printf("fuckyeahIcompile!!!\n");
-
- return 0;
-
+return 0;
 }
