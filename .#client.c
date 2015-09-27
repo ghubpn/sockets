1dea77ea6e534d6528f93f9d5f291f5ad751b193
nicholopolis <nhrivard@gmail.com>
2015-09-27 16:47:17 -0400

    client receive file

---
+++ .#client.c
@@ -0,0 +1,1 @@
+nhr@dhcp-90-248.cu-internet.carleton.ca.5637:1443290700
--- client.c
+++ client.c
@@ -5,6 +5,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <signal.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
 int main( int args, char *argv[] ) {
 // create socket
 int clisock;
@@ -14,8 +17,8 @@ clisock = socket( AF_INET, SOCK_STREAM, 0 );
 struct sockaddr_in servaddr;
 memset( &servaddr, 0, sizeof(servaddr) );
 servaddr.sin_family = AF_INET;
-servaddr.sin_port = htons(8888);
-servaddr.sin_addr.s_addr = inet_addr("0.0.0.0");
+servaddr.sin_port = htons(5432);
+servaddr.sin_addr.s_addr = inet_addr("172.17.63.49");
 //printf("yomamma\n");
 
 // connect to server once accepted
@@ -29,20 +32,84 @@ printf("connected\n");
 //keep communication with server 
 while(1){
 
-  printf("Enter commands\n");
-  char buf[6000];
+  //added on 21/09/2015 by Adam
+  printf("Please pick an option from the menu\n");
+  printf("-----------------------------------\n");
+  printf("[1] --- List the contents of the current directory\n"); //implements ls
+  printf("[2] --- Get a file from the server\n"); // implements get file-name
+  printf("[3] --- Send a file to the server\n"); //implements put file-name
+  printf("[4] --- Change Directory\n"); //implements cd
+  printf("[5] --- Make a new directory\n"); //implements mkdir directory-name
+  printf("Selection: ");
+  char buf[1];
+  char dirName[200];
+  char filename[200];
+  char readBuf[6000];
   scanf("%s", buf);
-  printf("\n");
-  ssize_t bytesSent = send( clisock, buf, strlen(buf), 0 );
-  memset(buf, 0, sizeof(buf));
-  if(bytesSent < 0){
-    printf("send failed");
-    return 1;
+
+  ssize_t bytesSent;
+  FILE *file;
+
+  if(buf[0] == '1'){
+    bytesSent = send( clisock, buf, strlen(buf), 0 );
+    printf("Listing.....\n");
+    
+  } 
+  else if(buf[0] == '2'){
+    bytesSent = send( clisock, buf, strlen(buf), 0 );
+    memset(buf, 0, sizeof(buf));
+    printf("Please Enter File Name To Receive\n");
+    scanf("%s", filename);
+    file = fopen(filename, "w");
+    if (file == NULL) {
+      printf("Could not open file.\n");
+      exit(1);
+    }
+    bytesSent = send( clisock, filename, strlen(filename), 0 );
+    memset(buf, 0, sizeof(buf));
+    if(recv(clisock, readBuf, 6000, 0) < 0){
+      printf("receive failed\n");
+      return 1;
+    }
+    fprintf(file, "%s", readBuf);
+    //fwrite(readBuf, 1, sizeof(readBuf), file);
+    //printf("Reading Buffer: %s\n", readBuf);
+    memset(readBuf, 0, sizeof(readBuf));
+    fclose(file);
+    printf("yup\n");
   }
-  printf("sent message of size %d\n", bytesSent);
+  else if(buf[0] == '3'){
+    bytesSent = send( clisock, buf, strlen(buf), 0 );
+    printf("Please Enter File Name To Send\n");
+  } 
+  else if(buf[0] == '4'){
+    bytesSent = send( clisock, buf, strlen(buf), 0 );
+    printf("Please Enter Directory Name\n");
+    scanf("%s", dirName);
+    bytesSent = send(clisock, dirName, strlen(dirName), 0);
+  } 
+  else if(buf[0] == '5'){
+    bytesSent = send( clisock, buf, strlen(buf), 0 );
+    printf("Please Enter new Directory Name\n");
+    scanf("%s", dirName);
+    bytesSent = send(clisock, dirName, strlen(dirName), 0);
+  } else{
+    printf("Not a corrent entry, please try again\n");
+  }
+  //editted up to here by Adam
+
+  printf("\n");
+   //bytesSent = send( clisock, buf, strlen(buf), 0 );
+  memset(buf, 0, sizeof(buf));
+  //printf("yup2\n");
+  /*if(bytesSent < 0){
+    printf("send failed");
+    //return 1;
+  }*/
+  //printf("sent message of size %d\n", bytesSent);
 
   char buf2[6000];
-
+  memset(buf2, 0, sizeof(buf2));
   if(recv(clisock, buf2, 6000, 0) < 0){
     printf("receive failed\n");
     return 1;
